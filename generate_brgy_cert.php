8c9e3da88872f2c47d3dd12dd3b575e25c99e82d
Marcelino Yu <yanmingrui27@gmail.com>
2023-01-15 18:44:54 +0800

    Changed to static payment values.

--- generate_brgy_cert.php
+++ generate_brgy_cert.php
@@ -221,7 +221,7 @@
                             <form method="POST" action="model/save_pment.php" >
                                 <div class="form-group">
                                     <label>Amount</label>
-                                    <input type="number" class="form-control" name="amount" placeholder="Enter amount to pay" required>
+                                    <input type="number" class="form-control" name="amount" value="50.00" readonly>
                                 </div>
                                 <div class="form-group">
                                     <label>Date Issued</label>
